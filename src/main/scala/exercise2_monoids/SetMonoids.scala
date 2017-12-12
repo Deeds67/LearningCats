761df88d0c5b8e1f467292a62d1ef0ed3482b6f5
Pierre Marais <pierrem@live.co.za>
2017-12-12 21:53:13 +0200

    Add setMonoids

--- src/main/scala/exercise2_monoids/SetMonoids.scala
+++ src/main/scala/exercise2_monoids/SetMonoids.scala
@@ -1,2 +1,23 @@
 package exercise2_monoids
 
+object SetMonoids {
+  implicit def setUnionMonoid[A]: Monoid[Set[A]] =
+    new Monoid[Set[A]] {
+      override def empty = Set.empty[A]
+
+      override def combine(a: Set[A], b: Set[A]) = a union b
+    }
+
+  implicit val intMonoid: Monoid[Int] =
+    new Monoid[Int] {
+      override def empty = 0
+
+      override def combine(a: Int, b: Int) = a + b
+    }
+
+  val intSetMonoid = Monoid[Set[Int]]
+
+  intSetMonoid.combine(Set(1, 2, 3), Set(1, 2, 4, 5, 6))
+
+  implicit def setIntersectionSemigroup[A]: Semigroup[Set[A]] = (a: Set[A], b: Set[A]) => a intersect b
+}
