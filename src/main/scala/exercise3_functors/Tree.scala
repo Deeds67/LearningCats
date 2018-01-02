381b28757c8bec87d625e70b741a6ee9c6ff3bd7
Pierre Marais <pierrem@live.co.za>
2018-01-02 01:54:04 -0500

    Add Main for testing Tree functor

--- src/main/scala/exercise3_functors/Tree.scala
+++ src/main/scala/exercise3_functors/Tree.scala
@@ -1,12 +1,17 @@
 package exercise3_functors
 
 import cats.Functor
+import cats.implicits._
 
 sealed trait Tree[+A]
 final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 final case class Leaf[A](value: A) extends Tree[A]
 
 object Tree {
+  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] =
+    Branch(left, right)
+  def leaf[A](value: A): Tree[A] =
+    Leaf(value)
   implicit val treeFunctor: Functor[Tree] = new Functor[Tree] {
     override def map[A, B](tree: Tree[A])(func: A => B): Tree[B] = {
       tree match {
@@ -17,10 +22,13 @@ object Tree {
       }
     }
   }
+}
 
-  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] =
-    Branch(left, right)
-  def leaf[A](value: A): Tree[A] =
-    Leaf(value)
+object Main extends App {
+  val tree = Tree.branch(Leaf(2), Leaf(4))
+  val tree2 = tree.map(_ * 2)
+
+  println(tree)
+  println(tree2)
 }
 
