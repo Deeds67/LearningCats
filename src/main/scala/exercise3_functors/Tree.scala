a375b7b0709f7f870779633797fed2c7fe46e05d
Pierre Marais <pierrem@live.co.za>
2018-01-02 01:43:34 -0500

    Implement functor for a Tree

---
+++ src/main/scala/exercise3_functors/Tree.scala
@@ -0,0 +1,26 @@
+package exercise3_functors
+
+import cats.Functor
+
+sealed trait Tree[+A]
+final case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+final case class Leaf[A](value: A) extends Tree[A]
+
+object Tree {
+  implicit val treeFunctor: Functor[Tree] = new Functor[Tree] {
+    override def map[A, B](tree: Tree[A])(func: A => B): Tree[B] = {
+      tree match {
+        case Branch(l, r) =>
+          Branch(map(l)(func), map(r)(func))
+        case Leaf(v) =>
+          Leaf(func(v))
+      }
+    }
+  }
+
+  def branch[A](left: Tree[A], right: Tree[A]): Tree[A] =
+    Branch(left, right)
+  def leaf[A](value: A): Tree[A] =
+    Leaf(value)
+}
+
